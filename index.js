38429bed50a062aea0404b328b0207684d5aaec5
Martin Di Martino-Marriott <m.dimartinomarriott@kainos.com>
2019-04-14 21:25:36 +0100

    Understanding promises

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const puppeteer = require('puppeteer');
 const fetch = require("node-fetch");
 
+// Safe IIFE
+// Out of global glope
+;(function(){
+    // ...
+})();
+
 (async () => { 
+
     const browser = await puppeteer.launch();
+    const artist = 'Metallica';
     const url = `https://en.wikipedia.org/wiki/Metallica_discography`;
     
     const getSinglesFromArtistWikiPage = async (url) => {
        const page = await browser.newPage();
        await page.goto(url);
-       
        const singles = await page.evaluate(() =>  
        Array.from(document.getElementById('Singles').parentElement.nextElementSibling.querySelectorAll('tr > th[scope="row"] > a'))
        .map( (single, i) =>  single.innerText )
@@ -16,34 +23,30 @@ const fetch = require("node-fetch");
        await page.close();
        return singles;
     }
-
-    const handleErrors = async (response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response;
-    }
-     
-    const getArtworkForTrack = async (artist, track) => {
-            return fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=440727626b4c31180a790ce8a03247d8&artist=${artist}&track=${track}&format=json`)
-                    .then( response => response.json() )
-                    .then(json =>  json.track.album.image[3]["#text"] )
-                    .catch(error =>  null  ) 
-    }
-
-    // Create object of Tracks and cover
-    const heavyMetalObject = async ( artist, tracks ) => {
-        const obj = {};
-        return tracks.map( (track) => {
-            const cover = getArtworkForTrack(artist, track); 
-            console.log(cover)
-            return (
-                cover
-                //{ ...tracks, cover }
-            )
-        })
-    }
+ 
 
     const tracks = await getSinglesFromArtistWikiPage(url);
-    console.log(heavyMetalObject('Metallica', tracks));
-    
+ 
+ 
+    const getArtworkForTracks = async (artist, tracks) => {
+        let artwork;
+        tracks.forEach( (track) => { 
+            fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=440727626b4c31180a790ce8a03247d8&artist=${artist}&track=${track}&format=json`)
+                .then( res => res.json() )
+                .then( data => data.track.album.image[3]["#text"])
+                .then( (cover) => {
+                    console.log(cover.split('300/')[1])
+                    artwork.push(res)
+                })
+                .catch(error =>  null)
+        })
+        return covers;
+    }
+
+
+    const covers = await getArtworkForTracks('Metallica', tracks);
+    console.log('covers:',covers);
+
+    //console.log( getArtistObject('Metallica', tracks) );
 
 })()
---
+++ promises.js
@@ -0,0 +1,130 @@
+// Practicing promises
+  
+/* start spinner
+fetch json
+add title
+fetch each chapter
+add story
+stop spinner
+*/ 
+
+function get(url) {
+    return new Promise( (resolve, reject) => {
+        var req = new XMLHttpRequest(); 
+        req.open('GET', url);
+        req.onload = function(){
+            if ( req.status == 200){
+                // resolve promise with response text
+                resolve(req.response)
+            } else {
+                reject( Error( req.statusText))
+            }
+        };
+        req.onerror = function(){
+            reject( Error('Network error'))
+        }; 
+        req.send();
+    })
+}
+
+/*
+get('data.json').then( (res) => {
+    console.log('sucess', res)
+}, (err) => {
+    console.log('error:', err)
+});
+*/
+
+// Transforming values
+var promise = new Promise( (res, rej) => {
+    res(4);
+});
+
+promise.then( (val) => {
+    return val*2;
+})
+// Pass in returned value
+.then( (val) => {
+    console.log(val);
+})
+
+getJson('data.json')
+    .then( (res) => {
+        console.log('JSON is', res);
+    })
+
+
+//Shortcut for Transforming JSON
+// getJSON() returns a promise;
+// fetches a url then parses the response as JSON.
+function getJson(url){
+    return get(url).then( JSON.parse )
+    .catch(err => {
+        console.log(`Get Json failed with error: ${err}`)
+        throw err
+    })
+}
+
+//Queuing asynchronous actions
+
+// Returning something promise-like
+// If a then callback returns another promise, the next then() waits on it being resolved
+
+getJson('data.json')
+.then( json => {
+    // Returns another url
+    return getJson(json[0].url)
+})
+// Waits until it's resolved...
+.then( (item) => {
+    console.log(`Got the item: ${item[1].extra_info}`);
+});
+
+let articlePromise;
+
+function getArticleInfo(i){
+    articlePromise = articlePromise || getJson('data.json') 
+    return articlePromise.then( (data) => {
+        return getJson( data[i].url )
+    })
+}
+
+// Function returns the same promise so it's reused, without calling it again
+getArticleInfo(0).then( (info) => {
+    console.log(info);
+    return getArticleInfo(1);
+})
+.then( (res) => {
+    console.log('res', res)
+})
+
+// Errors
+// Two args, success and failure.
+// (Fulfill and reject)
+
+// Two ways of specifying error callbacks
+get('data.json3').then( res => {
+    console.log(res)
+})
+.catch( (err)=> {
+    console.log('oops', err)
+})
+
+get('data.json4').then( (res) => {
+    console.log(res)
+}, (err) => {
+    console.log('oops again', err)
+})
+
+getJson('data.json').then( res => {  
+    return getJson(res[1].url)
+}).then( res => { 
+    document.write( res[0].extra_info)
+}).catch( () => {
+    let div = document.createElement('div')
+    div.innerHTML = 'failed'
+    div.style.color = 'red';
+    document.querySelector('body').appendChild(div);
+}).then( () => {
+   document.body.style.backgroundColor = 'lightgrey';
+})
